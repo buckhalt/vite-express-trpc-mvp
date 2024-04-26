fbc9fb522e42bccce2b3690b106325ee9d9171f3
Caden Buckhalt <halaeb@gmail.com>
2024-04-26 12:52:17 -0700

    test - removing base url from uploadthing

--- apps/web/src/components/uploadthing.ts
+++ apps/web/src/components/uploadthing.ts
@@ -8,7 +8,7 @@ import {
 import { type OurFileRouter } from '../../../server/src/uploadthing';
 
 const initOpts = {
-   url: `${import.meta.env.BACKEND_URL}/api/uploadthing`,
+   url: `/api/uploadthing`,
 } satisfies GenerateTypedHelpersOptions;
 
 export const UploadButton = generateUploadButton<OurFileRouter>(initOpts);
