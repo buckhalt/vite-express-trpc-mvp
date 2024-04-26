334f1d99b58d91268da5ca56043622dc8262ea49
Caden Buckhalt <halaeb@gmail.com>
2024-04-26 13:11:49 -0700

    use backend_url env variable in web app

--- apps/web/src/hooks/useTrpc.ts
+++ apps/web/src/hooks/useTrpc.ts
@@ -1,7 +1,7 @@
-import { trpc } from '@/trpc'
-import { QueryClient } from '@tanstack/react-query'
-import { httpLink } from '@trpc/client/links/httpLink'
-import { useState } from 'react'
+import { trpc } from '@/trpc';
+import { QueryClient } from '@tanstack/react-query';
+import { httpLink } from '@trpc/client/links/httpLink';
+import { useState } from 'react';
 
 export const useTrpc = () => {
    const [trpcQueryClient] = useState(
@@ -14,12 +14,14 @@ export const useTrpc = () => {
                },
             },
          })
-   )
+   );
 
-   const [trpcClient] = useState(() => trpc.createClient({ links: [httpLink({ url: '/trpc' })] }))
+   const [trpcClient] = useState(() =>
+      trpc.createClient({ links: [httpLink({ url: `${import.meta.env.BACKEND_URL}/trpc` })] })
+   );
 
    return {
       trpcQueryClient,
       trpcClient,
-   }
-}
+   };
+};
