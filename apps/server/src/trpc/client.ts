03c8d65819f3e9f3bb50aac6a2c1abe14730d721
Caden Buckhalt <halaeb@gmail.com>
2024-04-26 08:20:24 -0700

    change server url to server project

--- apps/server/src/trpc/client.ts
+++ apps/server/src/trpc/client.ts
@@ -2,6 +2,7 @@ import { createTRPCClient, httpLink } from '@trpc/client';
 
 import { type AppRouter } from './api/router';
 
-const serverUrl = process.env.NODE_ENV === 'production' ? 'https://studio-mvp.vercel.app' : 'http://localhost:3001';
+const serverUrl =
+   process.env.NODE_ENV === 'production' ? 'https://studio-mvp-server.vercel.app' : 'http://localhost:3001';
 
 export const client = createTRPCClient<AppRouter>({ links: [httpLink({ url: `${serverUrl}/trpc` })] });
